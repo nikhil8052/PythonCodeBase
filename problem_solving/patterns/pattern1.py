0abaf05f85c527a0ea80947adb5909409db0fd79
Nikhil <nikhilkumarnk1142@gmail.com>
2022-07-24 15:55:25 +0530

    Problem Solving

---
+++ problem_solving/patterns/pattern1.py
@@ -0,0 +1,24 @@
+'''
+Written By : Nikhil 
+Date : 23 JULY 2022 
+
+******
+******
+******
+******
+******
+******
+
+'''
+
+
+r=int(input(" Enter no of rows "))
+c=int(input(" Enter no of cols  "))
+
+for i in range(0,r+1):
+    for j in range(0,c+1):
+        print("*",end="")
+    print("")
+
+
+
